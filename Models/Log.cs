52a640a285b6db22630e580ced57a836b468c434
bsokat <barissokat@gmail.com>
2017-01-05 14:39:58 +0300

    Model classes added for all Filter Types

---
+++ Models/Log.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FilterTypes.Models
+{
+    public class Log
+    {
+        public string Controller { get; set; }
+        public string Action { get; set; }
+        public DateTime ProcessDate { get; set; }
+        public string Type { get; set; }
+    }
+}
---
+++ Models/LogsData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FilterTypes.Models
+{
+    public class LogsData
+    {
+        private static List<Log> _logs = new List<Log>();
+        public static List<Log> Logs
+        {
+            get { return _logs; }
+        }
+    }
+}
